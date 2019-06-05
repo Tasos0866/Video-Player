3155689cad94b5dec11363d3ceec3a3d4070a384
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-06-05 10:18:06 +0300

    Implemented volume and speed change functions.

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
-const ranges = player.querySelectorAll('player__slider');
+const ranges = player.querySelectorAll('.player__slider');
 
 /* Build the Functions */
 function togglePlay() {
@@ -25,6 +25,10 @@ function skip() {
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+}
+
 
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
@@ -32,3 +36,5 @@ video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
