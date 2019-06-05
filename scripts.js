9926a0dd3ea0753aae46f455d8e17ed5b8f9e6af
Tasos Tsoukalas <tasostsouk.tt2@gmail.com>
2019-06-05 09:48:43 +0300

    Assigned all the elements in variables and implemented play and pause video functions.

---
+++ scripts.js
@@ -0,0 +1,22 @@
+/* Get the Elements */
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('player__slider');
+
+/* Build the Functions */
+function togglePlay() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+
+/* Hook up the event listeners */
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
